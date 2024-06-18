3c0ec9ed1c7f86b05bd63f19981abcc06a006424
NaufalAmajid <naufalamajid@gmail.com>
2024-06-18 23:39:27 +0700

    finish menu data user

--- admin/classes/Pemilik.php
+++ admin/classes/Pemilik.php
@@ -1,5 +1,4 @@
 <?php
-date_default_timezone_set('Asia/Jakarta');
 
 class Pemilik
 {
@@ -8,6 +7,28 @@ class Pemilik
     public function __construct()
     {
         $this->conn = DB::getInstance()->connection();
+    }
+
+    public function addPemilik($data)
+    {
+        $db = DB::getInstance();
+        $res = $db->add('detail_pemilik', $data);
+        return $res;
+    }
+
+    public function updatePemilik($table, $data, $where)
+    {
+        $db = DB::getInstance();
+        $res = $db->update($table, $data, $where);
+        return $res;
+    }
+
+    public function addUser($data)
+    {
+        $db = DB::getInstance();
+        $result = $db->add('user', $data);
+        $user_id = $this->conn->lastInsertId();
+        return $user_id;
     }
 
     public function getAllPemilik()
@@ -31,7 +52,7 @@ class Pemilik
         return $data;
     }
 
-    public function getAllPemilikById($pemilik_id)
+    public function getPemilikById($pemilik_id)
     {
         $query = "SELECT
                         dp.*,
@@ -57,5 +78,74 @@ class Pemilik
         $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once '../config/connection.php';
+    require_once '../classes/DB.php';
+
+    $pemilik = new Pemilik();
+    if ($_POST['action'] == 'add_pemilik') {
+        foreach ($_POST['data'] as $field => $value) {
+            $data[$field] = $value;
+        }
+
+        $checkUser = $pemilik->checkUser($data['username']);
+        if ($checkUser) {
+            echo json_encode(['status' => 'error', 'message' => 'Username sudah digunakan oleh pemilik lain', 'icon' => 'bx bx-error-circle']);
+            exit;
+        }
+
+        $dataUser = [
+            'username' => $data['username'],
+            'password' => md5($data['password']),
+            'role_id' => 1,
+        ];
+        $addUser = $pemilik->addUser($dataUser);
+
+        $dataPemilik = [
+            'user_id' => $addUser,
+            'nama_pemilik' => $data['nama_pemilik'],
+        ];
+        $addPemilik = $pemilik->addPemilik($dataPemilik);
+
+        if ($addPemilik) {
+            echo json_encode(['status' => 'success', 'message' => 'Pemilik berhasil ditambahkan', 'icon' => 'bx bx-check-circle']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Pemilik gagal ditambahkan', 'icon' => 'bx bx-error-circle']);
+        }
+    }
+
+    if ($_POST['action'] == 'edit_pemilik') {
+
+        foreach ($_POST['data'] as $field => $value) {
+            $data[$field] = $value;
+        }
+
+
+        $checkUser = $pemilik->checkUser($data['username']);
+        if ($checkUser) {
+            if (intval($data['user_id']) != $checkUser['user_id']) {
+                echo json_encode(['status' => 'error', 'message' => 'Username sudah digunakan oleh pemilik lain', 'icon' => 'bx bx-error-circle']);
+                exit;
+            }
+        }
+
+        $dataEditUser = [
+            'username' => $data['username'],
+        ];
+        $updateUser = $pemilik->updatePemilik('user', $dataEditUser, ['user_id' => $data['user_id']]);
+
+        $dataEditPemilik = [
+            'nama_pemilik' => $data['nama_pemilik'],
+        ];
+        $updatePemilik = $pemilik->updatePemilik('detail_pemilik', $dataEditPemilik, ['pemilik_id' => $data['pemilik_id']]);
+
+        if ($updatePemilik || $updateUser) {
+            echo json_encode(['status' => 'success', 'message' => 'User dan pemilik berhasil diubah', 'icon' => 'bx bx-check-circle']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'User dan pemilik gagal diubah', 'icon' => 'bx bx-error-circle']);
+        }
     }
 }
--- admin/content/modal-pemilik.php
+++ admin/content/modal-pemilik.php
@@ -3,11 +3,10 @@ $action = isset($_POST['action']) ? $_POST['action'] : '';
 $title  = $action ? $action : 'baru';
 if ($action == 'detail') {
     require_once '../config/connection.php';
-    require_once '../config/functions.php';
-    require_once '../classes/Pelanggan.php';
+    require_once '../classes/Pemilik.php';
 
-    $pelanggan = new Pelanggan();
-    $pelanggan = $pelanggan->getAllPelangganById($_POST['pelanggan_id']);
+    $pemilik = new Pemilik();
+    $pemilik = $pemilik->getPemilikById($_POST['pemilik_id']);
 }
 ?>
 <div class="modal-dialog modal-dialog-centered">
@@ -16,11 +15,11 @@ if ($action == 'detail') {
             <h5 class="modal-title">Pemilik <?= ucwords($title) ?></h5>
         </div>
         <div class="modal-body">
-            <form id="form-add-pelanggan">
+            <form id="form-add-pemilik">
                 <div class="row mb-3">
                     <div class="col-lg-6 col-sm-12">
                         <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" required value="<?= $action ? $pelanggan['username'] : '' ?>">
+                        <input type="text" class="form-control" id="username" name="username" required value="<?= $action ? $pemilik['username'] : '' ?>">
                     </div>
                     <div class="col-lg-6 col-sm-12">
                         <label for="password" class="form-label">Password</label>
@@ -30,28 +29,8 @@ if ($action == 'detail') {
                 </div>
                 <div class="row mb-3">
                     <div class="col-lg-12 col-sm-12">
-                        <label for="nama_pelanggan" class="form-label">Nama Pelanggan</label>
-                        <input type="text" class="form-control" id="nama_pelanggan" name="nama_pelanggan" required value="<?= $action ? $pelanggan['nama_pelanggan'] : '' ?>">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-lg-6 col-sm-12">
-                        <label for="no_hp" class="form-label">No Hp</label>
-                        <input type="text" class="form-control" id="no_hp" name="no_hp" required value="<?= $action ? $pelanggan['no_hp'] : '' ?>">
-                    </div>
-                    <div class="col-lg-6 col-sm-12">
-                        <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                        <select class="form-control" id="jenis_kelamin" name="jenis_kelamin" required>
-                            <option value="">Pilih Jenis Kelamin</option>
-                            <option value="Laki-laki" <?= $action ? ($pelanggan['jenis_kelamin'] == 'Laki-laki' ? 'selected' : '') : '' ?>>Laki-laki</option>
-                            <option value="Perempuan" <?= $action ? ($pelanggan['jenis_kelamin'] == 'Perempuan' ? 'selected' : '') : '' ?>>Perempuan</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-lg-12 col-sm-12">
-                        <label for="alamat" class="form-label">Alamat</label>
-                        <textarea class="form-control" id="alamat" name="alamat" required><?= $action ? $pelanggan['alamat'] : '' ?></textarea>
+                        <label for="nama_pemilik" class="form-label">Nama Pemilik</label>
+                        <input type="text" class="form-control" id="nama_pemilik" name="nama_pemilik" required value="<?= $action ? $pemilik['nama_pemilik'] : '' ?>">
                     </div>
                 </div>
             </form>
@@ -59,10 +38,133 @@ if ($action == 'detail') {
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <?php if ($action) : ?>
-                <button type="button" onclick="editPelanggan('<?= $pelanggan['pelanggan_id'] ?>', '<?= $pelanggan['user_id'] ?>')" class="btn btn-warning">Update</button>
+                <button type="button" onclick="editPemilik('<?= $pemilik['pemilik_id'] ?>', '<?= $pemilik['user_id'] ?>')" class="btn btn-warning">Update</button>
             <?php else : ?>
-                <button type="button" onclick="addPelanggan()" class="btn btn-primary">Simpan</button>
+                <button type="button" onclick="addPemilik()" class="btn btn-primary">Simpan</button>
             <?php endif; ?>
         </div>
     </div>
 </div>
+<script>
+    function addPemilik() {
+        let data = $('#form-add-pemilik').serializeArray();
+        let send = {};
+        let cek = [];
+        data.map(item => {
+            if (item.value == '') {
+                cek.push(item.name);
+            } else {
+                send[item.name] = item.value;
+            }
+        })
+        if (cek.length != 0) {
+            let text = cek.join(', ');
+            Lobibox.notify("warning", {
+                pauseDelayOnHover: true,
+                size: "mini",
+                rounded: true,
+                delayIndicator: false,
+                delay: 2500,
+                icon: "bx bx-error",
+                continueDelayOnInactiveTab: false,
+                sound: false,
+                position: "center top",
+                msg: `Field ${text} harus diisi!`
+            });
+            return;
+        } else {
+            $.ajax({
+                url: 'classes/Pemilik.php',
+                type: 'POST',
+                data: {
+                    action: 'add_pemilik',
+                    data: send
+                },
+                success: function(response) {
+                    let res = JSON.parse(response);
+
+                    Lobibox.notify(`${res.status}`, {
+                        pauseDelayOnHover: true,
+                        size: "mini",
+                        rounded: true,
+                        delayIndicator: false,
+                        delay: 2500,
+                        icon: `${res.icon}`,
+                        continueDelayOnInactiveTab: false,
+                        sound: false,
+                        position: "center top",
+                        msg: `${res.message}`
+                    });
+
+                    if (res.status == 'success') {
+                        setTimeout(() => {
+                            location.reload();
+                        }, 2500);
+                    }
+                }
+            })
+        }
+    }
+
+    function editPemilik(pemilik_id, user_id) {
+        let data = $('#form-add-pemilik').serializeArray();
+        let send = {};
+        let cek = [];
+        data.map(item => {
+            if (item.value == '') {
+                cek.push(item.name);
+            } else {
+                send[item.name] = item.value;
+            }
+        })
+        send['pemilik_id'] = pemilik_id;
+        send['user_id'] = user_id;
+        if (cek.length != 0) {
+            let text = cek.join(', ');
+            Lobibox.notify("warning", {
+                pauseDelayOnHover: true,
+                size: "mini",
+                rounded: true,
+                delayIndicator: false,
+                delay: 2500,
+                icon: "bx bx-error",
+                continueDelayOnInactiveTab: false,
+                sound: false,
+                position: "center top",
+                msg: `Field ${text} harus diisi!`
+            });
+            return;
+        } else {
+            $.ajax({
+                url: 'classes/Pemilik.php',
+                type: 'POST',
+                data: {
+                    action: 'edit_pemilik',
+                    data: send
+                },
+                success: function(response) {
+                    let res = JSON.parse(response);
+
+                    Lobibox.notify(`${res.status}`, {
+                        pauseDelayOnHover: true,
+                        size: "mini",
+                        rounded: true,
+                        delayIndicator: false,
+                        delay: 2500,
+                        icon: `${res.icon}`,
+                        continueDelayOnInactiveTab: false,
+                        sound: false,
+                        position: "center top",
+                        msg: `${res.message}`
+                    });
+
+                    if (res.status == 'success') {
+                        setTimeout(() => {
+                            location.reload();
+                        }, 2500);
+                    }
+                }
+            })
+        }
+    }
+</script>
